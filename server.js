8a35dab7ea5b1a7c416d7844920ea43dbed60b3a
Mike L <mike.levy.rva@gmail.com>
2022-08-30 11:00:33 -0400

    added comment routes and index in api folder

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 //handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
