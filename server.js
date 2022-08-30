6369efb2f8d69c4ff8c985dbae917be783724a25
Mike L <mike.levy.rva@gmail.com>
2022-08-30 11:29:40 -0400

    commented in handlebar requirements and helpers

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 //handlebars
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({ helpers });
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,8 +32,8 @@ app.use(session(sess));
 
 app.use(routes);
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 // turn on connection to db and server
