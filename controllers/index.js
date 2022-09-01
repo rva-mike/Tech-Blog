ea1a39633e08f563071c8882e6bb021aba89a896
Mike L <mike.levy.rva@gmail.com>
2022-08-31 22:20:08 -0400

    heroku

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ const dashboardRoutes = require('./dashboard-routes.js');
 
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use('/home', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
