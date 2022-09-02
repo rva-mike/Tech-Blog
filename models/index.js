f132ea64abd20da7c4bf6da3d14ced03d1ef9f09
Mike L <mike.levy.rva@gmail.com>
2022-09-02 18:15:27 -0400

    deploy

--- models/index.js
+++ models/index.js
@@ -4,9 +4,6 @@ const User = require('./User');
 const Comment = require('./Comment');
 
 //create associations
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
@@ -16,13 +13,6 @@ Comment.belongsTo(User, {
     foreignKey: 'user_id'
   });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
